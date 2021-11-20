caeeb3bacea591b40820b1a9ac75f13aa71e1201
Vincent Tembo <vincenttembo@Vincents-MacBook-Pro.local>
2021-11-20 14:13:20 -0500

    added todos

--- contracts/VarietySavingsDAO.sol
+++ contracts/VarietySavingsDAO.sol
@@ -7,13 +7,16 @@ contract VarietySavingsDAO {
         uint32 roundNumber;
         address[] votingUsers;
         mapping(address => uint32) voteCount;
+        // TODO: add start date, end date
+        // TODO: bring tokens into the struct?
     }
     mapping(address => bool) addressVotingEligibity;
+    // TODO: keep symbol/name of tokens somewhere
     address[] availableTokens;
 
     VotingRound public votingRound;
-    
-    // make sure user can only vote once
+
+    // TODO: make sure user can only vote once
     function voteForTokens(address[] memory _chosenTokens) public {
         uint8 numberOfVotedTokens = uint8(_chosenTokens.length);
         for (uint8 i = 0; i < numberOfVotedTokens; i++) {
@@ -22,11 +25,13 @@ contract VarietySavingsDAO {
         }
     }
 
+    // TODO: Only callable internally and triggered by savings contract
     function setAddressEligibility(address user, bool eligibility) public {
         addressVotingEligibity[user] = eligibility;
     }
 
     function newVotingRound() public {
+        // TODO: distribute rewards
         uint64 numberPriorRoundVoters = uint64(votingRound.votingUsers.length);
         for (uint64 i = 0; i < numberPriorRoundVoters; i++) {
             address user = votingRound.votingUsers[i];
@@ -37,4 +42,10 @@ contract VarietySavingsDAO {
         delete votingRound.votingUsers;
         votingRound.roundNumber += 1;
     }
+
+    // TODO: give users the ability to vote for new tokens on the next round
+
+    // TODO: change tokens on the next round if necessary
+
+    // TODO: this contract should custody variety tokens?
 }
