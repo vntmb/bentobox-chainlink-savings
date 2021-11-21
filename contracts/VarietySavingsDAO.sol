56d52d834f7aed89c12db4bdc14cf36d7de87449
Vincent Tembo <vincenttembo@Vincents-MacBook-Pro.local>
2021-11-21 17:44:26 -0500

    distribute tokens

--- contracts/VarietySavingsDAO.sol
+++ contracts/VarietySavingsDAO.sol
@@ -2,27 +2,50 @@
 
 pragma solidity >=0.8.0;
 
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+
+// TODO: add emits to the contract
 contract VarietySavingsDAO {
     struct VotingRound {
         uint32 roundNumber;
         address[] votingUsers;
         mapping(address => uint32) voteCount;
+        mapping(address => bool) userVoted;
         // TODO: add start date, end date
         // TODO: bring tokens into the struct?
     }
     mapping(address => bool) addressVotingEligibity;
     // TODO: keep symbol/name of tokens somewhere
     address[] availableTokens;
+    uint8 TRANSFER_TOKEN_AMOUNT = 10;
 
     VotingRound public votingRound;
 
-    modifier onlyEligible() {
+    modifier eligibleToVote() {
         require(addressVotingEligibity[msg.sender], 'You are not eligible to vote');
         _;
     }
 
-    // TODO: make sure user can only vote once
-    function voteForTokens(address[] memory _chosenTokens) public onlyEligible {
+    modifier notVotedYet() {
+        require(!votingRound.userVoted[msg.sender], 'You can only vote once');
+        _;
+    }
+
+    function voteForTokens(address[] memory _chosenTokens) public eligibleToVote notVotedYet {
         uint8 numberOfVotedTokens = uint8(_chosenTokens.length);
         for (uint8 i = 0; i < numberOfVotedTokens; i++) {
             address currentToken = _chosenTokens[i];
@@ -35,11 +58,21 @@ contract VarietySavingsDAO {
         addressVotingEligibity[user] = eligibility;
     }
 
+    // TODO: use safe transfer
+    function distributeTokens(address user) private {
+        uint8 numberOfTokens = uint8(availableTokens.length);
+        for (uint8 i = 0; i < numberOfTokens; i++) {
+            IERC20 token = IERC20(availableTokens[i]);
+            token.transfer(user, TRANSFER_TOKEN_AMOUNT);
+        }
+    }
+
     function newVotingRound() public {
-        // TODO: distribute rewards
         uint64 numberPriorRoundVoters = uint64(votingRound.votingUsers.length);
         for (uint64 i = 0; i < numberPriorRoundVoters; i++) {
             address user = votingRound.votingUsers[i];
+            // distribute rewards
+            distributeTokens(user);
             // reset user's vote
             votingRound.voteCount[user] = 0;
         }
